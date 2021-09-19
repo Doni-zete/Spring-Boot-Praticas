d94fe3c7bc4fa7427e145bba3b76e0c77b6d58ec
Doni-zete <donizete112@gmail.com>
2021-09-18 23:58:30 -0300

    atualizando arquivos

--- spring-web-mvc/src/main/java/com/globallabs/springwebmvc/controller/JediController.java
+++ spring-web-mvc/src/main/java/com/globallabs/springwebmvc/controller/JediController.java
@@ -1,13 +1,31 @@
 package com.globallabs.springwebmvc.controller;
 
 
+import com.globallabs.springwebmvc.model.Jedi;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class JediController {
+
     @GetMapping("/jedi")
-    public String jedi(){
-        return "jedi";
+    public ModelAndView jedi(){
+
+        final  ModelAndView modelAndView =new ModelAndView();
+        modelAndView.setViewName("jedi");
+        final Jedi luke = new Jedi("Luke", "Skywalker");
+        modelAndView.addObject("allJedi", List.of (luke));
+
+        return  modelAndView;
     }
+
+    @GetMapping("new-jedi")
+    public String newJedi(){
+        return "new-jedi";
+    }
+
+
 }
