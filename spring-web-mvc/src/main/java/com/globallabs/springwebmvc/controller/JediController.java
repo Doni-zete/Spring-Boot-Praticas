03c40c6d2379c384f671b9c2ddf809f5273e3cba
Doni-zete <donizete112@gmail.com>
2021-09-22 22:46:26 -0300

    adicionando arquivos

--- spring-web-mvc/src/main/java/com/globallabs/springwebmvc/controller/JediController.java
+++ spring-web-mvc/src/main/java/com/globallabs/springwebmvc/controller/JediController.java
@@ -1,16 +1,16 @@
-package com.globallabs.springwebmvc.controller;
+package com.spring.web.controller;
 
-
-import com.globallabs.springwebmvc.model.Jedi;
-import com.globallabs.springwebmvc.repository.JediRepository;
+import com.spring.web.model.Jedi;
+import com.spring.web.repository.JediRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class JediController {
@@ -19,29 +19,33 @@ public class JediController {
     private JediRepository repository;
 
     @GetMapping("/jedi")
-    public ModelAndView jedi(){
+    public ModelAndView jedi() {
 
-        final  ModelAndView modelAndView =new ModelAndView();
+        final ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("jedi");
+        modelAndView.addObject("allJedi", repository.getAll());
 
-        modelAndView.addObject("allJedi", repository.getAllJedi());
-
-        return  modelAndView;
+        return modelAndView;
     }
 
     @GetMapping("/new-jedi")
-    public ModelAndView newJedi(){
-        final  ModelAndView modelAndView =new ModelAndView();
-        modelAndView.setViewName("new-jedi");
+    public String createJedi(Model model) {
 
-        modelAndView.addObject("jedi", new Jedi());
-        return modelAndView;
+        model.addAttribute("jedi", new Jedi());
+
+        return "new-jedi";
     }
-    @PostMapping("/jedi")
-    public String createJedi(@ModelAttribute Jedi jedi){
 
-        repository
+    @PostMapping("/jedi")
+    public String createJedi(@ModelAttribute Jedi jedi, BindingResult result, RedirectAttributes redirect) {
+
+        if (result.hasErrors()) {
+            return "new-jedi";
+        }
+        repository.add(jedi);
+        redirect.addFlashAttribute("message", "Jedi successfully created.");
+
+        return "redirect:jedi";
     }
 
 }
-
--- spring-web-mvc/src/main/java/com/globallabs/springwebmvc/model/Jedi.java
+++ spring-web-mvc/src/main/java/com/globallabs/springwebmvc/model/Jedi.java
@@ -1,31 +1,40 @@
-package com.globallabs.springwebmvc.model;
+package com.spring.web.model;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class Jedi {
-    private  String name;
-    private  String lastName;
 
-    public Jedi(final  String name, final String lastname) {
+
+    @Size(min = 3, max = 20, message = "Name must have between 3 and 20 letters")
+    private String name;
+    
+    @Size(max = 20, message = "Last Name must not have more than 20 letters")
+    private String lastName;
+
+    public Jedi (final String name, final String lastName) {
         this.name = name;
-        this.lastName = lastname;
+        this.lastName = lastName;
     }
 
-    public Jedi() {
-
+    public Jedi () {
+        
     }
 
-    public String getName() {
+    public String getName () {
         return name;
     }
 
-    public void setName(final String name) {
+    public void setName (final String name) {
         this.name = name;
     }
 
-    public String getLastName() {
+    public String getLastName () {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
+    public void setLastName (final String lastName) {
         this.lastName = lastName;
     }
+
 }
