6388c4988f06699943381ee3c9218844359ff13e
Doni-zete <donizete112@gmail.com>
2021-09-29 23:31:41 -0300

    repository

--- product-catalog/src/main/java/one/digitalinovation/experts/productcatallog/config/ElasticSearchConfig.java
+++ product-catalog/src/main/java/one/digitalinovation/experts/productcatallog/config/ElasticSearchConfig.java
@@ -1,4 +1,12 @@
 package one.digitalinovation.experts.productcatallog.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "one.digitalinnovation.experts.productcatalog.repository")
 public class ElasticSearchConfig {
+
+
+
 }
