8b3e494395ac23d68b58b121f0c3bf7a96b8b3c2
Doni-zete <donizete112@gmail.com>
2021-09-29 23:25:31 -0300

    adicionando arquivos

---
+++ product-catalog/src/main/java/one/digitalinovation/experts/productcatallog/ProductCatallogApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinovation.experts.productcatallog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatallogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatallogApplication.class, args);
+	}
+
+}
---
+++ product-catalog/src/test/java/one/digitalinovation/experts/productcatallog/ProductCatallogApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinovation.experts.productcatallog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatallogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
